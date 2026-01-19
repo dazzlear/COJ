891fdf1f350a6f89e1361b5d0e76ffd61273ad02
dazzlear <dazel.argallon7@gmail.com>
2026-01-20 02:05:50 +0800

    fix: music page

--- COJ/Controllers/.vshistory/HomeController.cs/2026-01-20_01_49_26_250.cs
+++ COJ/Controllers/.vshistory/HomeController.cs/2026-01-20_01_49_26_250.cs
@@ -21,8 +21,8 @@ public class HomeController : Controller
             {
                 new("Silakbo", "Album", "2025", "~/img/silakbo.jpg", "https://www.youtube.com/watch?v=r5eisDLrYeE"),
                 new("Sandali", "Single", "2025", "~/img/sandali.jpg", "https://www.youtube.com/watch?v=PGaUaT15Ei4"),
-                new("Misteryoso", "Single", "2024", "~/img/misteryoso.jpg", "https://www.youtube.com/watch?v=Svm0vY91oN0&list=RDSvm0vY91oN0&start_radio=1"),
-                new("Lahat Ng Bukas", "Single", "2024", "~/img/lahatngbukas.jpg", "https://www.youtube.com/watch?v=70IsaoX9Hdg&list=RD70IsaoX9Hdg&start_radio=1")
+                new("Misteryoso", "Single", "2024", "~/img/misteryoso.jpg", ""),
+                new("Lahat Ng Bukas", "Single", "2024", "~/img/lahatngbukas.jpg", "")
             }
         };
 
